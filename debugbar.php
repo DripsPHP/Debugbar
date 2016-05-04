930c344c9f4f2ac815420285da85544ccd9054f2
Raffael Kessler <raffael.kessler@prowect.com>
2016-05-04 21:29:43 +0200

    Auf-/Zuklappen

--- debugbar.php
+++ debugbar.php
@@ -3,26 +3,44 @@
 
 <div id="debugbar">
     <header id="debugbar-header">
-        <nav style="float: right;">
-            <a href="#tab1">A</a>
-            <a href="#tab2">B</a>
-            <a href="#tab3">C</a>
-        </nav>
         <nav>
-            <a href="#tab1">A</a>
-            <a href="#tab2">B</a>
-            <a href="#tab3">C</a>
+            <a href="#debugbartab-A">A</a>
+            <a href="#debugbartab-B">B</a>
+            <a href="#debugbartab-C">C</a>
         </nav>
     </header>
     <div id="debugbar-content">
-        <div class="tab">
+        <div class="tab" id="debugbartab-A">
             Tab 1
         </div>
-        <div class="tab">
+        <div class="tab" id="debugbartab-B">
             Tab 2
         </div>
-        <div class="tab">
+        <div class="tab" id="debugbartab-C">
             Tab 3
         </div>
     </div>
 </div>
+
+<script>
+  window.jQuery || document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"><\/script>')
+</script>
+
+<script>
+$(document).ready(function(){
+    $("#debugbar-content .tab").eq(0).show();
+
+    $("#debugbar-header a").click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        id = $(this).prop("href").substr($(this).prop("href").indexOf("#"));
+        $("#debugbar-content .tab").hide();
+        $("#debugbar-content").show();
+        $(id).show();
+    });
+
+    $("#debugbar-header").click(function(e){
+        $("#debugbar-content").toggle();
+    });
+});
+</script>
