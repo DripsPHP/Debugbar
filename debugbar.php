0ab47f24190edb120af24c914cc560777a27cd02
sorrina <1981maariaa@gmail.com>
2016-05-04 20:39:08 +0200

    design

---
+++ debugbar.php
@@ -0,0 +1,28 @@
+
+    <link a href="style_debugbar.css" rel="stylesheet">
+
+<div id="debugbar">
+    <header id="debugbar-header">
+        <nav style="float: right;">
+            <a href="#tab1">A</a>
+            <a href="#tab2">B</a>
+            <a href="#tab3">C</a>
+        </nav>
+        <nav>
+            <a href="#tab1">A</a>
+            <a href="#tab2">B</a>
+            <a href="#tab3">C</a>
+        </nav>
+    </header>
+    <div id="debugbar-content">
+        <div class="tab">
+            Tab 1
+        </div>
+        <div class="tab">
+            Tab 2
+        </div>
+        <div class="tab">
+            Tab 3
+        </div>
+    </div>
+</div>
