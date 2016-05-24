9ac1d9bf7ab3095bcd3e8a0898ffaa5abf024fd5
lars-c-mueller <lars.c.mueller@gmail.com>
2016-05-24 17:52:49 +0200

    Dump-Tests hinzugefügt

--- index.php
+++ index.php
@@ -2,15 +2,28 @@
 
 include("vendor/autoload.php");
 
-use Drips\Debugbar\Debugbar;
+error_reporting(E_ALL);
+
+use Drips\Debugger;
+// use Drips\Debugbar\Debugbar;
 
 Debugbar::on("create", function($bar){
     $bar->registerTab("test", "TestTab", "<h1>Works!</h1>");
     $bar->appendTab("test", "<h1>Works!</h1>");
     $bar->registerTab("test", "TestTab", "<h1>Works!</h1>");
     $bar->registerTab("test2", "2. Tab", "<h1>Works ebenso!</h1>");
+    $bar->registerTab("dumper", "Dump it", "<h1>lelelele ebenso!</h1>");
+    //$bar->registerTab("dump", "dump", "<h1>lelelele ebenso!</h1>");
+    $bar->appendTab("dumper", "<h1>Works!</h1>");
     $bar->registerInfo("currentdate", date("d.m.Y"));
     $bar->registerInfo("currentweek", date("W"));
 });
+
+/*
+$arr = array(['affe', 13]);
+echo dump($arr);
+dump("TEAST");
+*/
+
 
 echo Debugbar::getInstance();
