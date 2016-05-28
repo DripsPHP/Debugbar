139123bae4c7b3a16298f826f4626b35ead9fe8c
Raffael Kessler <raffael.kessler@prowect.com>
2016-05-29 00:28:46 +0200

    config.php entfernt

--- drips.php
+++ drips.php
@@ -4,6 +4,10 @@ use Drips\App;
 use Drips\HTTP\Request;
 use Drips\Debugbar\Debugbar;
 
+if(!defined('DRIPS_DEBUG')){
+    define('DRIPS_DEBUG', true);
+}
+
 if(class_exists('Drips\App')){
     App::on('shutdown', function(){
         if(in_array("text/html", (new Request)->getAccept())){
