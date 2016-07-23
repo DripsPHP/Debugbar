06cf86938365ce4904eecaf6d2a1beb31d3ca728
sorrina <kathisag@hotmail.com>
2016-07-23 21:27:01 +0200

    DocBlocks und Formatierung

--- src/Debugbar.php
+++ src/Debugbar.php
@@ -5,6 +5,12 @@ namespace Drips\Debugbar;
 use Drips\Utils\Event;
 use Drips\Utils\OutputBuffer;
 
+
+/**
+ * Class Debugbar.
+ *
+ * Erweiterbare Debugbar zum Debuggen von PHP Webanwendungen
+ */
 class Debugbar
 {
     use Event;
@@ -29,6 +35,11 @@ class Debugbar
         return static::$instance;
     }
 
+    /**
+     * speichert die Ausgabe
+     *
+     * @return mixed
+     */
     public function __toString()
     {
         $buffer = new OutputBuffer;
@@ -37,11 +48,24 @@ class Debugbar
         return $buffer->end();
     }
 
+    /**
+     * neue Tabs registrieren
+     *
+     * @param string $name
+     * @param string $title
+     * @param string content
+     *
+     */
     public function registerTab($name, $title, $content='')
     {
         $this->tabs[$name] = array(static::TITLE => $title, static::CONTENT => $content);
     }
 
+    /**
+     * erweitert bestehenden Tab
+     * @param string $name
+     * @param string $content
+     */
     public function appendTab($name, $content){
       if(array_key_exists($name, $this->tabs)){
         $this->tabs[$name][static::CONTENT].=$content;
@@ -50,59 +74,127 @@ class Debugbar
       return false;
     }
 
+    /**
+     * neue Info registrieren
+     *
+     * @param string $name
+     * @param string $info
+     */
     public function registerInfo($name, $info)
     {
         $this->infos[$name] = $info;
     }
 
+    /**
+     * alle registrierten Tabs
+     *
+     * @return array
+     */
     public function getTabs()
     {
         return $this->tabs;
     }
 
+    /**
+     * alle registrierten Infos
+     *
+     * @return array
+     */
     public function getInfos()
     {
         return $this->infos;
     }
 
+    /**
+     * prüft ob Infos registriert wurden
+     *
+     * @return bool
+     */
     public function hasInfos(){
         return !empty($this->infos);
     }
 
+    /**
+     * benennt Info um
+     *
+     * @param string $name
+     * @param string $new_name
+     */
     public function setInfo($name, $new_name){
         $this->registerInfo($name, $new_name);
     }
 
+    /**
+     * ändert Namen vom Tab
+     *
+     * @param string $name
+     * @param string $new_name
+     */
     public function setTabTitle($name, $new_name){
         if($this->hasTab($name)){
             $this->tabs[$name][static::TITLE] = $new_name;
         }
     }
 
+    /**
+     * prüft ob Info registriert wurde, gibt den Namen zurück
+     *
+     * @param string $name
+     * @return mixed
+     */
     public function getInfo($name){
         if($this->hasInfo($name)){
             return $this->infos[$name];
         }
     }
 
+    /**
+     * prüft ob Infos registriert wurden
+     *
+     * @param string $name
+     * @return bool
+     */
     public function hasInfo($name){
         return isset($this->infos[$name]);
     }
 
+    /**
+     * prüft, ob der Tab bereits registriert wurde
+     *
+     * @param string $name
+     * @return bool
+     */
     public function hasTab($name){
         return isset($this->tabs[$name]);
     }
 
+    /**
+     * prüft ob bereits Tabs registriert wurden
+     *
+     * @return bool
+     */
     public function hasTabs(){
         return !empty($this->tabs);
     }
 
+    /**
+     * prüft ob Tab registriert wurde und gibt Namen zurück
+     *
+     * @param string $name
+     * @return mixed
+     */
     public function getTabTitle($name){
         if($this->hasTab($name)){
             return $this->tabs[$name][static::TITLE];
         }
     }
 
+    /**
+     * püft ob Tab registriert wurde und gibt Inhalt zurück
+     *
+     * @param int $name
+     * @return mixed
+     */
     public function getTabContent($name){
         if($this->hasTab($name)){
             return $this->tabs[$name][static::CONTENT];
